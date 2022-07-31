8cb6f4ccd9c7aec6765beb4c0aa896e3ffcbec4a
bavabala <62609269+bavabala@users.noreply.github.com>
2022-07-31 14:24:33 +0530

    Add files via upload
    accept the string and count the number of upper and lower case in given string

---
+++ upper_lowercase.py
@@ -0,0 +1,19 @@
+# Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+
+#function:
+def string_case(string):
+    case = {"UPPER_CASE":0,"LOWER_CASE":0}
+
+#for loop:
+    for char in string:
+        if char.isupper():
+            case["UPPER_CASE"]+=1
+        elif char.islower():
+            case["LOWER_CASE"]+=1
+        else:
+            pass
+#print the upper and lower case:
+    print("Original string :",string)
+    print("number of upper case characters : ",case["UPPER_CASE"])
+    print("number of lower case characters : ",case["LOWER_CASE"])
+string_case('The quick Brow Fox')
